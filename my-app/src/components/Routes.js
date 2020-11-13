9a601a59844f6f06b7779fde1810b2a973d345bc
Ben Brumby <bbrumby00@gmail.com>
2020-11-12 21:26:57 -0500

    trying to make this work

--- my-app/src/components/Routes.js
+++ my-app/src/components/Routes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Index from "../../pages/Index";
-import Portfolio from "../../pages/Portfolio";
-import Contact from "../../pages/Contact";
+import Main from "../pages/Main";
+import Portfolio from "../pages/Portfolio";
+import Contact from "../pages/Contact";
 
 const Routes = () => {
     return (
         <Switch> 
-            <Route exact path = "/" component ={Index}></Route>
+            <Route exact path = "/" component ={Main}></Route>
             <Route exact path = "/portfolio" component = {Portfolio}></Route>
             <Route exact path = "/contact" component = {Contact}></Route>
         </Switch>
