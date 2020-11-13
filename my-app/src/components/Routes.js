7a4782989f1c66390b71122ee0a2f45cd862c280
Ben Brumby <bbrumby00@gmail.com>
2020-11-12 21:24:59 -0500

    another attempt

--- my-app/src/components/Routes.js
+++ my-app/src/components/Routes.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Index from "../pages/Index";
-import Portfolio from "../pages/Portfolio";
-import Contact from "../pages/Contact";
+import Index from "../../pages/Index";
+import Portfolio from "../../pages/Portfolio";
+import Contact from "../../pages/Contact";
 
 const Routes = () => {
     return (
