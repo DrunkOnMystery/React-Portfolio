597b670ec8e2255b37c037fafc5d2108b73e0096
Ben Brumby <bbrumby00@gmail.com>
2020-11-13 19:37:27 -0500

    fixedissue

--- my-app/src/components/BodyPortfolio.js
+++ my-app/src/components/BodyPortfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Project from "../../models/Portfolio";
+// import Project from "../../models/Portfolio";
 import '../styles/BodyPortfolio.css';
 
 function BodyPortfolio() {
