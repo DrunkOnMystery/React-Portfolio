1143fbade0ec13a9c5789de4ef187240e13ac97c
Ben Brumby <bbrumby00@gmail.com>
2020-11-15 11:28:43 -0500

    trying to add my background page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Routes from "./components/Routes";
+import './styles/style.css';
 
 function App() {
   return (
     <div className="App">
+      <div class="app">
       <Header />
       <Routes />
       <Footer />
+      </div>
     </div>
   );
 }
