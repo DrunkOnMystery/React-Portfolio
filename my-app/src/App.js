64862aa192cc146c43c8d922230a559838983e1c
Ben Brumby <bbrumby00@gmail.com>
2020-11-15 11:11:54 -0500

    fixed problem in app.js file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import './styles/App.css';
 import Routes from "./components/Routes";
 
 function App() {
