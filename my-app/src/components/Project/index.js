ac7f0919c599d9f4485f6aa63805b6aa04ad4ef3
Ben Brumby <bbrumby00@gmail.com>
2020-12-29 12:40:46 -0500

    updating projects file

--- my-app/src/components/Project/index.js
+++ my-app/src/components/Project/index.js
@@ -6,8 +6,10 @@ function Project(props) {
                 class="card-img-top" alt={props.title} /></a>
             <div class="card-body">
                 <h5 class="card-title">{props.title}</h5>
-                <p class="card-text">{props.description} <a target="_blank" rel="noreferrer"
-                        href={props.github}>Github here.</a></p>
+                <p class="card-text">{props.description}</p>
+                <p> <a target="_blank" rel="noreferrer"
+                        href={props.github}>Github here.</a><br /></p>
+                        <p>This project was built using {props.technologies}.</p>
             </div>
         </div>
     )
