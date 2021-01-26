9e434929a9f3271959b82e5e2c0420ed5e5881e2
Ben Brumby <bbrumby00@gmail.com>
2021-01-26 12:30:48 -0500

    Fixed portfolio json issue

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,7 +6,9 @@ function Project(props) {
                 class="card-img-top" alt={props.title} /></a>
             <div class="card-body">
                 <h5 class="card-title">{props.title}</h5>
-                <p class="card-text">{props.description} <a target="_blank" rel="noreferrer"
+                <p class="card-text">{props.description} 
+                The following technologies were used: {props.technologies}
+                <a target="_blank" rel="noreferrer"
                         href={props.github}>Github here.</a></p>
             </div>
         </div>
